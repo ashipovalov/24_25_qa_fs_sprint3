c8f099f0b2cebb467d8421332756b36844fce540
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-05-13 20:14:02 +0300

    Для мерджа

--- src/main/java/ru/practicum/SpeedLight.java
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -4,7 +4,9 @@ package ru.practicum;
 public class SpeedLight {
 
     public String showLight(int currentSpeed) {
+
         if (currentSpeed < 60) {
+            System.out.println("Демонстрация решения коммита через merge");
             return "green";
         } else if (currentSpeed < 80) {
             return "yellow";
