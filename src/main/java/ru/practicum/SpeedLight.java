b23aa4b3bbb71044917e8d97e7f90d6183167fea
Александр Шиповалов <ashipovalov@gmail.com>
2025-05-13 19:22:19 +0300

    Добавлен метод для тестов

---
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+
+public class SpeedLight {
+
+    public String showLight(int currentSpeed) {
+        if (currentSpeed < 60) {
+            return "green";
+        } else if (currentSpeed < 80) {
+            return "yellow";
+        } else {
+            return "red";
+        }
+    }
+}
