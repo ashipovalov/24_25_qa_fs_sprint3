b4ca242737b25c32d476f189adcec000eb23f536
Александр Шиповалов <ashipovalov@gmail.com>
2025-05-13 19:37:04 +0300

    Добавлены зависимость от Junit и написаны первые тесты

--- src/test/java/ru/practicum/tests/SpeedLightTest.java
+++ src/test/java/ru/practicum/tests/SpeedLightTest.java
@@ -1,4 +1,29 @@
 package ru.practicum.tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.practicum.SpeedLight;
+
 public class SpeedLightTest {
+
+    @Test
+    public void checkLightGreen() {
+        SpeedLight speedLight = new SpeedLight();
+        Assert.assertEquals("green", speedLight.showLight(59));
+
+    }
+
+    @Test
+    public void checkLightYellow() {
+        SpeedLight speedLight = new SpeedLight();
+        Assert.assertEquals("yellow", speedLight.showLight(79));
+    }
+
+    @Test
+    public void checkLightRed() {
+        SpeedLight speedLight = new SpeedLight();
+        Assert.assertEquals("red", speedLight.showLight(99));
+
+    }
+
 }
