007b0cf361a462586f070f3bcb545dc00b4d3a2a
Александр Шиповалов <ashipovalov@gmail.com>
2025-05-13 20:16:45 +0300

    Для решения конфликта через merge

--- src/main/java/ru/practicum/SpeedLight.java
+++ src/main/java/ru/practicum/SpeedLight.java
@@ -5,6 +5,7 @@ public class SpeedLight {
 
     public String showLight(int currentSpeed) {
         if (currentSpeed < 60) {
+            System.out.println("А это для коммита в Идее");
             return "green";
         } else if (currentSpeed < 80) {
             return "yellow";
